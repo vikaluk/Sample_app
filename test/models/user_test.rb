7d933ce5311b67d8aede0f5d45f7c0a16d697f13
vikaluk <vikaluk@gmail.com>
2019-04-07 08:51:18 +0300

    Adv login

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,7 +4,10 @@ class UserTest < ActiveSupport::TestCase
 
   def setup
     @user = User.new(name: "Example User", email: "user@example.com")
-password: "foobar", password_confirmation: "foobar")
+    password: "foobar", password_confirmation: "foobar")
+  end
+  test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?('')
   end
 
   test "should be valid" do
